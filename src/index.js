cc89bb4a5a5eeaa2a656d81bc74cc763d0689db9
wlisses <wlissesfernando285@gmail.com>
2023-05-19 20:06:36 -0300

    testando venom

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const venon = require('venom-bot');
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-  res.status(200).json({ message: 'hell world'});
-});
+async function main() {
+  const client = venon.create({
+    session: "test-session",
+    headless: false,
+    useChrome: false
+  });
+};
 
-
+main();
 app.listen(3001, () => console.log("Servidor rodando na porta 3001"));
