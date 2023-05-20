43961b13b5463705f3714d98404bba75ac2c3a9a
wlisses <wlissesfernando285@gmail.com>
2023-05-20 09:59:49 -0300

    tentando encontra uma forma que funcione

--- api/cron/index.js
+++ api/cron/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const venom = require('venom-bot')
 
 const app = express();
-
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.status(200).json({ message: 'hello world' });
-});
+async function main() {
+  try {
+    const client = await venom.create({
+      session: "test_cron",
+      headless: true,
+      useChrome: false
+    });
+   const verify = await client.sendText("5531998566526@c.us", "hello")
+   console.log(verify);
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+main();
 
 app.listen(3001, () => console.log("Servidor executando"));
